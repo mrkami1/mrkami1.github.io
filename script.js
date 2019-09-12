a67d040fe9487be0b07d517d1b9f1cf9ec983da7
Mashhood <mashhood.anwar@gmail.com>
2019-09-12 15:35:45 -0400

    added animations

--- script.js
+++ script.js
@@ -1,12 +1,56 @@
 let underline;
-let aboutMeButton, projectsButton, contactButton;
+let aboutMeButton, projectsButton, contactButton, buttonClass, buttonsDiv;
 
 window.onload = function() {
+    //Get all the elements
     underline = document.getElementById("underline");
     projectsButton = document.getElementById("projects_button");
     aboutMeButton = document.getElementById("about_button");
     contactButton = document.getElementById("contact_button");
+    buttonClass = document.getElementsByClassName("button");
+    buttonsDiv = document.getElementById("buttons");
+    
+    //Start the page load animation
+    loadAnimation();
+
+    //Init button clicks and setup button animations
     projectsButton.onclick = function() {
         console.log("hellooooo");
     }
+    for (let i = 0; i < buttonClass.length; i++) {
+        buttonClass.item(i).onmousedown = function() { 
+            this.style.backgroundColor = "rgb(90, 90, 90)";
+            this.style.color = "white"
+        }
+        buttonClass.item(i).onmouseup = function() { 
+            this.style.backgroundColor = "white";
+            this.style.color = "black" 
+        }
+        buttonClass.item(i).onmouseenter = function() { 
+            this.style.backgroundColor = "white";
+            this.style.color = "black"  
+        }
+        buttonClass.item(i).onmouseleave = function() { 
+            this.style.backgroundColor = "gray";
+            this.style.color = "white"  
+        }
+    }
+    
+}
+function loadAnimation() {
+    anime({
+        targets: underline,
+        width: '50%', // -> from '28px' to '100%',
+        easing: 'easeInOutQuad',
+        duration: 800,
+        loop: false
+      });
+    anime({
+        targets: buttonsDiv,
+        opacity: 100,
+        easing: 'easeInOutQuad',
+        duration: 10000,
+        delay: 800,
+        loop: false
+    });
 }
